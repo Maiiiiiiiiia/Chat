bb7f888bd13ae510c724a43945de8ce42688d268
Maia Kulieva <hi.maiakulieva@gmail.com>
2024-05-27 23:25:43 +0400

    fix dropdown

--- frontend/src/components/channels/Channels.jsx
+++ frontend/src/components/channels/Channels.jsx
@@ -74,36 +74,32 @@ return (
               <span className="visually-hidden">{t('channels.button.plus')}</span>
             </Button>
       </div>
-
-      {/* <div className="flex-column nav-fill px-2 mb-3 overflow-auto h-100 d-block nav"> */}
       <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
-      
         {channels.map((channel, index) => (
           <li className='nav-item w-100' key={channel.id}>
-            <div role="group" className="d-flex dropdown btn-group">
+            <Dropdown as={ButtonGroup} drop="down" className="w-100">
+            {/* <div role="group" className="d-flex dropdown btn-group"> */}
               <button 
                   type="button" 
                   className={`w-100 rounded-0 text-start btn ${
                     channel.id === currentChannelId ? 'btn-secondary' : ''
                   }`}
                   onClick={() => switchChannel(channel)}
-                >
-                  <span className='me-1'>#</span>
-                  {' '}
-                  {channel.name}
-              </button>
-              {index >= 2 && (
-                  <Dropdown as={ButtonGroup}>
-                  <Dropdown.Toggle split variant="bg-light" id={channel.id}>
+                > 
+                 <span className='me-1'>#{' '}{channel.name}</span>
+                </button>
+                {index >= 2 && (
+                  <>
+                  <Dropdown.Toggle as={Button} className="text-end" split variant="bg-light" id={`dropdown-split-button${channel.id}`} >
                     <span className="visually-hidden">{t('modals.channelManagement')}</span>
                   </Dropdown.Toggle>
                   <Dropdown.Menu>
                     <Dropdown.Item id={channel.id} onClick={() => setShowModal('removing', { id: channel.id })}>{t('channels.button.delete')}</Dropdown.Item>
                     <Dropdown.Item id={channel.id} name={channel.name} onClick={() => setShowModal('renaming', { id: channel.id, name: channel.name })}>{t('channels.button.rename')}</Dropdown.Item>
                   </Dropdown.Menu>
+                  </>
+                )}
                 </Dropdown>
-              )}
-              </div>
           </li>
         ))}
       </ul>
